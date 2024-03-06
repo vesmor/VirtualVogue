458c0e030cbefcb48467c89ac3cc567cb18f58a4
Alexandra Ramlogan Salgado <aramlogan02@gmail.com>
2024-03-06 18:12:50 -0500

    ADDED SERVER.JS TO GITIGNORE! SEE DESCRIPTION
    Due to the difference between ports for Mac and Windows and Linux, i added the server.js file to the gitignore. This allows for differences between os systems. PING ME ON DISCORD AND ALERT ME OF ANY SERVER CHANGES!

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5001));
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) =>
