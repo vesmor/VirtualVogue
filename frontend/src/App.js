b9ea3885bf1337ae8170f3557803cb33ea6670bf
deandrehendrix <deandrehendrix96@gmail.com>
2024-03-06 21:39:28 -0500

    delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
-import LandingPage from "./LandingPage"; // import the LandingPage component
+import LandingPage from "./LandingPage";
 import "./styles.css";
-// import Login from "./Login"; // import the Login component
 
 function App() {
   return (
@@ -12,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
