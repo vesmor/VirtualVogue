5e14282578fcb84fa6cca69b803f3eb7697fabff
deandrehendrix <deandrehendrix96@gmail.com>
2024-03-07 19:04:51 -0500

    modify signinfirst page

--- frontend/src/SignInFirst.js
+++ frontend/src/SignInFirst.js
@@ -1,35 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useRef } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Image,
+} from "react-bootstrap";
 import "./styles.css";
 
 function SignInFirst() {
   const navigate = useNavigate();
-  const gifContainerRef = useRef();
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://tenor.com/embed.js";
-    script.async = true;
-    gifContainerRef.current.appendChild(script);
-  }, []);
+  const gifUrl = "https://media.tenor.com/0t1tbXP8VJIAAAAi/no-emotiguy.gif";
 
   return (
-    <div className="signin-first">
-      <div
-        ref={gifContainerRef}
-        className="tenor-gif-embed"
-        data-postid="15194421034682700946"
-        data-share-method="host"
-        data-aspect-ratio="1"
-        data-width="100%"
-      >
-        <a href="https://tenor.com/view/no-emotiguy-emotiguy-no-mad-emotiguy-emotiguy-mad-gif-15194421034682700946">
-          No Emotiguy Sticker
-        </a>
-        from <a href="https://tenor.com/search/no-stickers">No Stickers</a>
-      </div>
-      <button onClick={() => navigate("/signin")}>Go to Sign In</button>
-    </div>
+    <Container fluid className="signin-first d-flex flex-column vh-100">
+      <Row className="flex-grow-1 d-flex">
+        <Col className="d-flex justify-content-center align-items-center">
+          <Image src={gifUrl} className="no-image" />{" "}
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center align-items-center">
+          <button onClick={() => navigate("/signin")}>Go to Sign In</button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
