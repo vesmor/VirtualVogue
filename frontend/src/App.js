cdc36a2ab94d2d5beabe08b743e08993537c6747
L7R-a <gedylarosa@gmail.com>
2024-03-09 02:09:37 -0500

    Started the design for the sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
       </Routes>
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  Form
+} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Signup.css';
 
 const Signup = () => {
   const [login, setLogin] = useState("");
@@ -61,62 +69,145 @@ const Signup = () => {
   };
 
   return (
-    <div id="loginDiv">
-      <form onSubmit={doRegister}>
-        <span id="inner-title">PLEASE SIGN UP</span>
-        <br />
-        <input
-          type="text"
-          id="idLogin"
-          placeholder="Login"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          id="idFirstName"
-          placeholder="First name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          id="idLastName"
-          placeholder="Last name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          id="idEmail"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          id="idPassword"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          id="idConfirmPassword"
-          placeholder="Confirm password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <br />
-        <input type="submit" id="loginButton" className="buttons" value="Do It" />
-      </form>
-    </div>
+<Container fluid className="signupContainer">
+      <Row>
+        <Col md={6}>
+
+        </Col>
+
+        <Col md={6} className="signup-form">
+          <Form onSubmit={doRegister}>
+
+            <h2 className = "signupTitle">SIGN UP</h2>
+
+            {/* First name field */}
+            <Form.Group controlId="idFirstName">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                 type="text"
+                 placeholder="First name"
+                 value={firstName}
+                 onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Form.Group>
+
+            {/* Last name field */}
+            <Form.Group controlId="idLastName">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </Form.Group>
+
+            {/* Email field */}
+            <Form.Group controlId="idEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            {/* Login ID field */}
+            <Form.Group controlId="idLogin">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}
+              />
+            </Form.Group>
+
+            {/* Password field */}
+            <Form.Group controlId="idPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            {/*Confirm Password field */}
+            <Form.Group controlId="idConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                 type="password"
+                 placeholder="Confirm password"
+                 value={confirmPassword}
+                 onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Form.Group>
+            <p className = "errorSignUp">User already Exist or bad password</p>
+            <Button type="submit" id="signupButton" className="signupButtonsPage">Signup</Button>
+          <hr></hr>
+          </Form>
+          <p>Already have an account?</p>
+          <Button type="submit" id="loginButton" className="signupButtonsPage">Login</Button>
+
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default Signup;
+
+            {/* <span id="inner-title">PLEASE SIGN UP</span>
+            <br />
+            <input
+              type="text"
+              id="idLogin"
+              placeholder="Login"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              id="idFirstName"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              id="idLastName"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              id="idEmail"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <input
+              type="password"
+              id="idPassword"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <input
+              type="password"
+              id="idConfirmPassword"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <br /> */}
+            
