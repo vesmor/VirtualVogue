0f564163f61e416e8095b54d7f5be17ecbbd1275
L7R-a <gedylarosa@gmail.com>
2024-03-09 22:50:00 -0500

    Removed signup from first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
       </Routes>
