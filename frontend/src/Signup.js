2a6ee803ab1a2cd3b1e65a263be7847141549445
jmauv <mauvaisjonathan@gmail.com>
2024-03-13 15:02:09 -0400

    testing heroku stuff

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -25,7 +25,7 @@ const Signup = () => {
   {
     if(process.env.NODE_ENV === 'production')
     {
-      return 'https://' + app_name + '.herokuapp.com' + route;
+      return 'https://' + app_name + '.herokuapp.com/' + route;
     }
     else
     {
